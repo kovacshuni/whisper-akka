135070197289954bd1e1d48d25f181fef01b907d
Hunor Kovács <hunor.kovacs@ft.com>
2014-09-04 00:29:49 +0100

    Same problem with futures.

--- src/main/scala/com/example/Whisper.scala
+++ src/main/scala/com/example/Whisper.scala
@@ -1,17 +1,21 @@
 package com.example
 
+import java.util.concurrent.{TimeUnit, Executors}
+
 import akka.actor.{ActorSystem, Props, ActorRef, Actor}
 
 import scala.collection.mutable.ListBuffer
-import scala.concurrent.{Await, Promise}
+import scala.concurrent.{ExecutionContext, Await, Promise}
 import scala.concurrent.duration._
 
 object Whisper {
 
   private val NWhisperers = 1000000
   private val system = ActorSystem("my-actor-system")
+  private val executor = Executors.newSingleThreadExecutor()
+  implicit private val ec = ExecutionContext.fromExecutorService(executor)
 
-  def main(args: Array[String]): Unit = {
+  def main2(args: Array[String]): Unit = {
     val gameOver = Promise[Boolean]()
     val lastWhisperer = system.actorOf(Props(classOf[LastWhisperer], gameOver), name = "last-whisperer")
     var latestWhisperer: ActorRef = lastWhisperer
@@ -26,6 +30,31 @@ object Whisper {
     Await.ready(gameOver.future, 3.0 seconds)
 
     println("Ended in " + (System.currentTimeMillis() - timeA) + " ms.")
+    system.shutdown()
+    system.awaitTermination(3.0 second)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val firstWhisperer = Promise[Int]()
+    var latestWhisperer = firstWhisperer
+    for (i <- NWhisperers to 1 by -1) {
+      val whisperer = Promise[Int]()
+      latestWhisperer.future onSuccess {
+        case n: Int => whisperer.success(n+1)
+      }
+      latestWhisperer = whisperer
+    }
+    println("Starting...")
+    val timeA = System.currentTimeMillis()
+
+    firstWhisperer.success(0)
+    println(Await.result(latestWhisperer.future, 3.0 seconds))
+
+    println("Ended in " + (System.currentTimeMillis() - timeA) + " ms.")
+    ec.shutdown()
+    ec.awaitTermination(3, TimeUnit.SECONDS)
+    executor.shutdown()
+    executor.awaitTermination(3, TimeUnit.SECONDS)
     system.shutdown()
     system.awaitTermination(3.0 second)
   }
