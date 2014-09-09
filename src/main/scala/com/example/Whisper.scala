328562bcf614555a33f484c5d0bba674ce07b056
Hunor Kovács <hunor.kovacs@ft.com>
2014-09-09 17:11:02 +0300

    Removed unnecessary code.

--- src/main/scala/com/example/Whisper.scala
+++ src/main/scala/com/example/Whisper.scala
@@ -2,63 +2,15 @@ package com.example
 
 import java.util.concurrent.{TimeUnit, Executors}
 
-import akka.actor.{ActorSystem, Props, ActorRef, Actor}
+import akka.actor.{ActorRef, Actor}
 
-import scala.collection.mutable.ListBuffer
 import scala.concurrent.{ExecutionContext, Await, Promise}
 import scala.concurrent.duration._
 
 object Whisper {
 
-  private val system = ActorSystem("my-actor-system")
   private val executor = Executors.newSingleThreadExecutor()
   implicit private val ec = ExecutionContext.fromExecutorService(executor)
-
-  def main2(args: Array[String]): Unit = {
-    val nWhisperers = args(0).toLong
-    val gameOver = Promise[Boolean]()
-    val lastWhisperer = system.actorOf(Props(classOf[LastWhisperer], gameOver), name = "last-whisperer")
-    var latestWhisperer: ActorRef = lastWhisperer
-    for (i <- nWhisperers to 1 by -1) {
-      val newWhisperer = system.actorOf(Props(classOf[Whisperer], latestWhisperer), name = s"whisperer-$i")
-      latestWhisperer = newWhisperer
-    }
-    println("Starting...")
-    val timeA = System.currentTimeMillis()
-
-    latestWhisperer ! 0
-    Await.ready(gameOver.future, 3.0 seconds)
-
-    println("Ended in " + (System.currentTimeMillis() - timeA) + " ms.")
-    system.shutdown()
-    system.awaitTermination(3.0 second)
-  }
-
-  def main3(args: Array[String]): Unit = {
-    val nWhisperers = args(0).toLong
-    val firstWhisperer = Promise[Int]()
-    var latestWhisperer = firstWhisperer
-    for (i <- nWhisperers to 1 by -1) {
-      val whisperer = Promise[Int]()
-      latestWhisperer.future onSuccess {
-        case n: Int => whisperer.success(n+1)
-      }
-      latestWhisperer = whisperer
-    }
-    println("Starting...")
-    val timeA = System.currentTimeMillis()
-
-    firstWhisperer.success(0)
-    println(Await.result(latestWhisperer.future, 3.0 seconds))
-
-    println("Ended in " + (System.currentTimeMillis() - timeA) + " ms.")
-    ec.shutdown()
-    ec.awaitTermination(3, TimeUnit.SECONDS)
-    executor.shutdown()
-    executor.awaitTermination(3, TimeUnit.SECONDS)
-    system.shutdown()
-    system.awaitTermination(3.0 second)
-  }
 
   def main(args: Array[String]): Unit = {
     val nWhisperers = args(0).toLong
@@ -80,8 +32,6 @@ object Whisper {
     ec.awaitTermination(3, TimeUnit.SECONDS)
     executor.shutdown()
     executor.awaitTermination(3, TimeUnit.SECONDS)
-    system.shutdown()
-    system.awaitTermination(3.0 second)
   }
 }
 
